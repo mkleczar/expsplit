ac5aabd7efc7814f4b0e972c60fac8ef96848961
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2023-07-25 14:35:48 +0200

    impl

---
+++ expsplit-backend/src/main/java/mk/expsplit/backend/ExpsplitBackendApplication.java
@@ -0,0 +1,13 @@
+package mk.expsplit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpsplitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpsplitBackendApplication.class, args);
+    }
+
+}
---
+++ expsplit-backend/src/test/java/mk/expsplit/backend/ExpsplitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.expsplit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpsplitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
